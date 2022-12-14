7306dcb1c34f1bd993326ad5f18c074e23d7a14c
virmanis1 <virmanisagar04@gmail.com>
2022-12-14 04:59:55 -0500

    new dropdown added

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -35,6 +35,10 @@ function myFunction() {
 function myFunction2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
